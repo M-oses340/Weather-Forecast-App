97e375d8e82ddad4d13ee565ced5775b856c3e60
M-oses340 <mosesomwa7@gmail.com>
2023-05-12 15:52:48 +0300

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,40 @@
 import React  from "react";
+import { useEffect,useState } from "react";
+import { useRouter } from "next/router";
+import { magic } from "../lib/magic-client";
 import "../styles/main.scss";
+import Loading from "../components/loading";
 
 function MyApp ({ Component , pageProps}){
-  return <Component { ...pageProps}/>
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoggedIn = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn();
+      if (isLoggedIn) {
+        // route to /
+        router.push("/");
+      } else {
+        // route to /login
+        router.push("/login");
+      }
+    };
+    handleLoggedIn();
+  }, []);
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false);
+    };
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
+
+  return isLoading ? <Loading /> : <Component {...pageProps} />;
 }
 export default MyApp;
--- pages/login.js
+++ pages/login.js
@@ -4,11 +4,32 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import styles from "../styles/";
 import styles from "../styles/Login.module.css";
+import { magic } from "../lib/magic-client";
+
+import { useState, useEffect } from "react";
+
 
 const Login = () => {
     const [email, setEmail] = useState("");
     const [userMsg, setUserMsg] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
     const router = useRouter();
+
+    useEffect(() => {
+        const handleComplete = () => {
+          setIsLoading(false);
+        };
+        router.events.on("routeChangeComplete", handleComplete);
+        router.events.on("routeChangeError", handleComplete);
+    
+        return () => {
+          router.events.off("routeChangeComplete", handleComplete);
+          router.events.off("routeChangeError", handleComplete);
+        };
+    }, [router]);
+
+
+
     const handleOnChangeEmail = (e) => {
       setUserMsg("");
       console.log("event", e);
@@ -20,11 +41,36 @@ const Login = () => {
     const handleLoginWithEmail = async (e) => {
         console.log("hi button");
         e.preventDefault();
+        setIsLoading(true);
 
         if (email) {
-          // route to dashboard
+            if (email === "mosesomwa98@gmail.com") {
+                // route to dashboard
+                router.push("/");
+                //  log in a user by their email
+                try {
+                  const didToken = await magic.auth.loginWithMagicLink({ email, });
+                  console.log({ didToken });
+                    if (didToken) {
+                        setIsLoading(false);
+                        router.push("/");
+                    }
+                } catch (error) {
+                  // Handle errors if required!
+                  console.error("Something went wrong logging in", error);
+                  setIsLoading(false);
+                }
+                // router.push("/");
+            } else {
+                console.log("Something went wrong logging in");
+                setIsLoading(false);
+                setUserMsg("Something went wrong logging in");
+                
+
+            }
         } else {
           // show user message
+          setIsLoading(false);
           setUserMsg("Enter a valid email address");
         }
 
@@ -67,6 +113,7 @@ const Login = () => {
                 <p className={styles.userMsg}>{userMsg}</p>
                 <button onClick={handleLoginWithEmail} className={styles.loginBtn}>
                   Sign In
+                  {isLoading ? "Loading..." : "Sign In"}
                 </button>
             </div>
         </main>
